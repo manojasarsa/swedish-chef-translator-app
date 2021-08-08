9f8cb677888cc4e086187e9252ab860a77e3f754
Manoj Asarsa <manojasarsa7611@gmail.com>
2021-08-08 17:40:26 +0530

    docs: Added styling

---
+++ app.js
@@ -0,0 +1,33 @@
+var btnTranslate = document.querySelector("#btn-translate");
+
+var inputText = document.querySelector("#input-Text");
+
+var outputDiv = document.querySelector("#output");
+
+var serverURL = "https://api.funtranslations.com/translate/chef.json"
+
+function getTranslationURL(input) {
+    return serverURL + "?" + "text=" + input
+}
+
+function errorHandler(error) {
+
+    console.log("error occurred", error);
+    alert("Something wrong with the server! Try again after some time")
+}
+
+function clickHandler() {
+
+    var inputValue = inputText.value;  // taking input
+
+    fetch(getTranslationURL(inputValue))
+        .then(response => response.json())
+        .then(json => {
+            
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText;
+        })
+        .catch(errorHandler)
+}
+
+btnTranslate.addEventListener("click", clickHandler)
